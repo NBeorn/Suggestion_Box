aeb5b0692afcfb3210d701afafd832027087a6d9
NBeorn <nmbeorn@gmail.com>
2017-10-24 14:43:54 -0400

    complete

---
+++ Suggestion_Box/Models/SuggestionModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Suggestion_Box.Models
+{
+    public class SuggestionModel
+    {
+        [Key]
+        public int ID { get; set; }
+
+        public string Topic { get; set; }
+
+        public string Comment { get; set; }
+
+        //Build after changing a model
+    }
+}
